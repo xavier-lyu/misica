3df62978be595964ff364b0b7c89f6988d8215dc
Xavier Lyu <xavier.lyu@protonmail.com>
2022-06-26 04:27:56 +0800

    refactor: simplify homepage data fetching and refreshing

--- lib/src/music/core/presentation/home_page.dart
+++ lib/src/music/core/presentation/home_page.dart
@@ -1,65 +1,124 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:misica/src/core/presentation/app_router.gr.dart';
 import 'package:misica/src/core/shared/theme_context.dart';
 import 'package:misica/src/localization/app_localizations_context.dart';
+import 'package:misica/src/music/charts/domain/charts.dart';
 import 'package:misica/src/music/charts/presentation/charts_list.dart';
+import 'package:misica/src/music/charts/shared/providers.dart';
 import 'package:misica/src/music/core/presentation/hook_scroll_view.dart';
+import 'package:misica/src/music/recommendations/domain/recommendation.dart';
 import 'package:misica/src/music/recommendations/presentation/recommendations_list.dart';
+import 'package:misica/src/music/recommendations/shared/providers.dart';
 
 import 'app_navbar.dart';
 
-class HomePage extends HookWidget {
+class HomePage extends StatefulHookConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
   static const _compactAppBarHeight = 44.0;
+  Completer? _completer;
+
+  @override
+  void initState() {
+    super.initState();
+    _handleRefreshing();
+  }
+
+  @override
+  void dispose() {
+    _completer?.complete();
+    _completer = null;
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final scrollOffset = useState<double>(0);
 
+    ref.listen<AsyncValue<Charts>>(
+      chartsNotifierProvider,
+      (prev, next) {
+        if (_completer?.isCompleted == false) {
+          next.whenOrNull(data: (_) => _completer?.complete());
+        }
+      },
+    );
+
+    ref.listen<AsyncValue<List<Recommendation>>>(
+      recosNotifierProvider,
+      (previous, next) {
+        if (_completer?.isCompleted == false) {
+          next.whenOrNull(
+            data: (_) => _completer?.complete(),
+            error: (error, _) => _completer?.completeError(error),
+          );
+        }
+      },
+    );
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(bottom: 66.0),
-        child: HookScrollView(
-          onOffsetChanged: (offset) => scrollOffset.value = offset,
-          slivers: [
-            AppNavbar(
-              title: Row(
-                mainAxisAlignment: scrollOffset.value < _compactAppBarHeight
-                    ? MainAxisAlignment.spaceBetween
-                    : MainAxisAlignment.center,
-                children: [
-                  Text(
-                    context.loc.home,
-                    style: context.ttoc.titleLarge,
-                    textAlign: TextAlign.start,
-                  ),
-                  if (scrollOffset.value < _compactAppBarHeight)
-                    const SettingsActionWidget()
+        child: RefreshIndicator(
+          onRefresh: _handleRefreshing,
+          edgeOffset: 140,
+          child: HookScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            onOffsetChanged: (offset) => scrollOffset.value = offset,
+            slivers: [
+              AppNavbar(
+                title: Row(
+                  mainAxisAlignment: scrollOffset.value < _compactAppBarHeight
+                      ? MainAxisAlignment.spaceBetween
+                      : MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      context.loc.home,
+                      style: context.ttoc.titleLarge,
+                      textAlign: TextAlign.start,
+                    ),
+                    if (scrollOffset.value < _compactAppBarHeight)
+                      const SettingsActionWidget()
+                  ],
+                ),
+                centerTitle: scrollOffset.value >= _compactAppBarHeight,
+                actions: [
+                  if (scrollOffset.value >= _compactAppBarHeight)
+                    const SettingsActionWidget(),
                 ],
               ),
-              centerTitle: scrollOffset.value >= _compactAppBarHeight,
-              actions: [
-                if (scrollOffset.value >= _compactAppBarHeight)
-                  const SettingsActionWidget(),
-              ],
-            ),
-            const SliverPadding(
-              padding: EdgeInsetsDirectional.only(
-                top: 15,
+              const SliverPadding(
+                padding: EdgeInsetsDirectional.only(
+                  top: 15,
+                ),
+                sliver: RecommendationsList(),
               ),
-              sliver: RecommendationsList(),
-            ),
-            const SliverPadding(
-              padding: EdgeInsetsDirectional.only(top: 15),
-              sliver: ChartsList(),
-            ),
-          ],
+              const SliverPadding(
+                padding: EdgeInsetsDirectional.only(top: 15),
+                sliver: ChartsList(),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> _handleRefreshing() async {
+    _completer = Completer();
+    ref.read(recosNotifierProvider.notifier).fetchRecommendations();
+    ref.read(chartsNotifierProvider.notifier).fetchTopCharts();
+    return _completer!.future;
   }
 }
 
--- lib/src/music/recommendations/presentation/recommendations_list.dart
+++ lib/src/music/recommendations/presentation/recommendations_list.dart
@@ -1,60 +1,35 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:misica/src/music/core/presentation/divider_widget.dart';
 import 'package:misica/src/music/recommendations/presentation/recommendation_widget.dart';
 import 'package:misica/src/music/recommendations/shared/providers.dart';
 
-class RecommendationsList extends StatefulHookConsumerWidget {
+class RecommendationsList extends ConsumerWidget {
   const RecommendationsList({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _RecommendationsListState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(recosNotifierProvider);
 
-class _RecommendationsListState extends ConsumerState<RecommendationsList> {
-  @override
-  void initState() {
-    super.initState();
-    Future.microtask(() {
-      ref.read(recosNotifierProvider.notifier).fetchRecommendations();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final scrollOffset = useState<double>(0);
-    final scrollController = useScrollController();
-    scrollController.addListener(() {
-      scrollOffset.value = scrollController.offset;
-    });
-
-    return Consumer(
-      builder: ((context, ref, child) {
-        final state = ref.watch(recosNotifierProvider);
-
-        return state.maybeWhen(
-          orElse: () => SliverToBoxAdapter(
-            child: Container(),
-          ),
-          data: (recos) => SliverList(
-            delegate: SliverChildBuilderDelegate(
-              ((context, index) {
-                if (index.isOdd) {
-                  return const DividerWidget(
-                    endIndent: 0,
-                  );
-                }
-                return RecommendationWidget(recommendation: recos[index ~/ 2]);
-              }),
-              childCount: max(0, recos.length * 2 - 1),
-            ),
-          ),
-        );
-      }),
+    return state.maybeWhen(
+      orElse: () => SliverToBoxAdapter(
+        child: Container(),
+      ),
+      data: (recos) => SliverList(
+        delegate: SliverChildBuilderDelegate(
+          ((context, index) {
+            if (index.isOdd) {
+              return const DividerWidget(
+                endIndent: 0,
+              );
+            }
+            return RecommendationWidget(recommendation: recos[index ~/ 2]);
+          }),
+          childCount: max(0, recos.length * 2 - 1),
+        ),
+      ),
     );
   }
 }
